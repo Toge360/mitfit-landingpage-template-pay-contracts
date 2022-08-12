41623a9d0a6b7d2847e5e1ede6166d45e25a5168
Tobias Gerlach <tobias@gerlach360.de>
2022-08-12 11:20:02 +0200

    add MDI + Scroll-To Listener

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
 // every dependency has to be imported here
 // css, scss, image, fonts etc.
 import bulma from "./assets/scss/bulma.scss"; // main bulma styles
+import "@mdi/font/css/materialdesignicons.min.css"; // MD-Icons (https://pictogrammers.github.io/@mdi/font/5.3.45/)
 
-console.log("Hallo Welt!");
-console.log("Tschüss")
+/* Action-Button
+Auf Klick scrollt die Page zum #plugin DIV */
+const scrollTarget = document.getElementById('plugin');
+const actionButtons = document.getElementsByClassName("action-button");
+
+for (let button of actionButtons) {
+  button.addEventListener("click", function(e) {
+    scrollTarget.scrollIntoView({ left: 0, block: 'start', behavior: 'smooth' });
+    e.preventDefault();
+  });
+}
+
